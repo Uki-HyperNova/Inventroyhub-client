7313fa9f0c6f49074d148aa88f729c107dd68b64
Anokithen <anokithen@gmail.com>
2026-06-30 15:47:28 +0530

    prodect data axios finished

--- app/staff/dashboard/data.tsx
+++ app/staff/dashboard/data.tsx
@@ -1,14 +1,38 @@
-const  data = [
+import { getProducts } from "@/services/product"
+import { Product } from "@/types/product"
+import { useEffect, useState } from "react"
+
+// const  data = [
   
-  {
-    "id": 68,
-    "header": "Service Level Agreements",
-    "type": "Legal",
-    "status": "Done",
-    "target": "26",
-    "limit": "29",
-    "reviewer": "Assign reviewer"
+//   {
+//     "id": 68,
+//     "header": "Service Level Agreements",
+//     "type": "Legal",
+//     "status": "Done",
+//     "target": "26",
+//     "limit": "29",
+//     "reviewer": "Assign reviewer"
+//   }
+// ]
+const [data,setData] = useState<Product[]>([])
+
+const productdata = async () =>{
+
+  try{
+    const  response = await getProducts()
+    setData(response.products)
+  } catch (error){
+    console.error("Error ")
   }
-]
+}
+
+useEffect(()=>{
+  productdata()
+},[])
+
+
+
+
+
 
 export default data
